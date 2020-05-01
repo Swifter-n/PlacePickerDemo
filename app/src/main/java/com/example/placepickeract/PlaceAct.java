a21e072857afa33766f1b4b5990b4ceab210e1e2
Swifter-n <ipankthedoors@gmail.com>
2020-05-01 22:03:43 +0700

    add

---
+++ app/src/main/java/com/example/placepickeract/PlaceAct.java
@@ -0,0 +1,66 @@
+package com.example.placepickeract;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+public class PlaceAct extends AppCompatActivity {
+
+    TextView text_picker;
+    Button btn_picker;
+    int PLACE_PICKER_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place);
+
+        btn_picker = findViewById(R.id.btn_picker);
+        text_picker = findViewById(R.id.text_picker);
+
+        btn_picker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    startActivityForResult(builder.build(PlaceAct.this), PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                StringBuilder stringBuilder = new StringBuilder();
+                String latitude = String.valueOf(place.getLatLng().latitude);
+                String longitude = String.valueOf(place.getLatLng().longitude);
+                stringBuilder.append("Latitude: ");
+                stringBuilder.append(latitude);
+                stringBuilder.append("\n");
+                stringBuilder.append("Longitude: ");
+                stringBuilder.append(longitude);
+                text_picker.setText(stringBuilder.toString());
+            }
+        }
+
+    }
+}
